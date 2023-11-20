884e74debb247399eb529725861fbf87de396cf1
Brooke McWilliams <brmc230@gmail.com>
2023-11-20 14:26:07 -0500

    Added more features to the main menu screen

---
+++ Assets/Scripts/VolumeSlider.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    public TextMeshProUGUI numberText;
+    private Slider slider;
+
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        setNumberText(slider.value);
+    }
+
+    public void setNumberText(float value)
+    {
+        numberText.text = value.ToString();
+    }
+}
